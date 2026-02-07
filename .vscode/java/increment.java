153fa2dfcb4f9091e8a5f93af9fe9a3445255e31
isarivelans-04 <isarivelans@gmail.com>
2026-02-07 11:32:58 +0530

    Add Java sample programs for operators and increment
    Add three Java sample files under .vscode/java: increment.java demonstrates pre- and post-increment/decrement behavior; oper.java demonstrates integer division cast to float (the cast occurs after integer division); and a placeholder control statements/conditional stat.java is added for future conditional examples. Note: the placeholder file contains spaces in the package/class name and will need renaming to compile.

---
+++ .vscode/java/increment.java
@@ -0,0 +1,11 @@
+public class increment {
+    public static void main(String ar[]){
+        int n=5;
+    System.out.println(++n);
+    System.out.println(n);
+    System.out.println(n++);
+    System.out.println(n);
+    System.out.println(n--);
+    System.out.println(n);
+    }
+}
---
+++ .vscode/java/oper.java
@@ -0,0 +1,10 @@
+public class oper {
+    public static void main(String arg[]){
+    
+        int a=10;
+        int b=7;
+        float c=(float) (a/b);
+        System.out.print(c);
+    }
+    
+}
